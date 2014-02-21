e650f3e847e4bc1b20729ec581aa885ffc4e8ae6
spline <reticulatingspline@github.com>
2014-02-21 17:28:19 -0500

    Update init.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 ###
-# Copyright (c) 2012-2013, spline
+# Copyright (c) 2012-2014, spline
 # All rights reserved.
 ###
 
@@ -25,10 +25,13 @@ __contributors__ = {}
 # This is a url where the most recent plugin package can be downloaded.
 __url__ = '' # 'http://supybot.com/Members/yourname/Weather/download'
 
-import config
-import plugin
+from . import config
+from . import plugin
+from imp import reload
+# In case we're being reloaded.
 reload(config)
-reload(plugin) # In case we're being reloaded.
+reload(plugin)
+
 # Add more reloads here if you add third-party modules and want them to be
 # reloaded when this plugin is reloaded.  Don't forget to import them as well!
 
