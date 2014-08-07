a0a6c2313537d8f6496159357a9015426b4b5510
GLolol <GLolol1@hotmail.com>
2014-08-07 12:10:42 -0700

    Randomness: trole sum moar!!1!1!ONE!1!1TWO

--- Randomness/plugin.py
+++ Randomness/plugin.py
@@ -25,8 +25,20 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
 ###
+
+#  ________  ________   __  ____  ____________  ______  ____________
+# /_  __/ / / / ____/  / / / / / /_  __/  _/  |/  /   |/_  __/ ____/
+#  / / / /_/ / __/    / / / / /   / /  / // /|_/ / /| | / / / __/
+# / / / __  / /___   / /_/ / /___/ / _/ // /  / / ___ |/ / / /___
+#/_/ /_/ /_/_____/   \____/_____/_/ /___/_/  /_/_/  |_/_/ /_____/
+#  __________  ____  __    __       _____ __________  ________  ______
+# /_  __/ __ \/ __ \/ /   / /      / ___// ____/ __ \/  _/ __ \/_  __/
+#  / / / /_/ / / / / /   / /       \__ \/ /   / /_/ // // /_/ / / /
+# / / / _, _/ /_/ / /___/ /___    ___/ / /___/ _, _// // ____/ / /   ??
+#/_/ /_/ |_|\____/_____/_____/   /____/\____/_/ |_/___/_/     /_/
+#
+# Use at your own risk!
 
 import supybot.utils as utils
 from supybot.commands import *
@@ -36,12 +48,11 @@ import supybot.ircmsgs as ircmsgs
 import supybot.ircdb as ircdb
 import supybot.callbacks as callbacks
 import random
+from supybot.utils.structures import TimeoutQueue
 try:
     from supybot.i18n import PluginInternationalization
     _ = PluginInternationalization('Randomness')
 except ImportError:
-    # Placeholder that allows to run the plugin on a bot
-    # without the i18n module
     _ = lambda x:x
 
 class Randomness(callbacks.Plugin):
@@ -49,48 +60,99 @@ class Randomness(callbacks.Plugin):
     This should describe *how* to use this plugin."""
     threaded = True
     
+    def __init__(self, irc):
+        self.__parent = super(Randomness, self)
+        self.__parent.__init__(irc)
+        self.dotCounter = TimeoutQueue(10)
+    ##
+    # SHHHHHHHHHHH DON'T SPOIL THE JOKES DOWN HERE
+    ##
+    # OR I WILL BE VERY MAD AND FIND YOU
+    ##
+    # *insert ridiculous amount of code here*
+    ##
+    # ..why are you still here? >_>
+    ##
     def doPrivmsg(self, irc, msg):
-        if self.registryValue("enable") and \
-            irc.network.lower() == "overdrive-irc" and \
-            "wow" in irc.state.channels[msg.args[0]].ops and \
-            ircutils.stripFormatting(msg.args[1].lower()).startswith("wow"):
+        if self.registryValue("enable"):
             dots = "." * random.randint(0,10) # added emphasis...............
-            wowResponses1 = ["what is it",
-                            "hi %s%s" % (msg.nick, dots),
-                            "o/",
-                            "HI %s%s" % (msg.nick.upper(), dots),
-                            "go away -_-",
-                            "FFS",
-                            "ffs i'm trying to work",
-                            "WHAT DO YOU WANT",
-                            "leave me alone :|",
-                            "ugh",
-                            "pls",
-                            "stop" + dots,
-                            "stop it!",
-                            "reproted to fbi for harrassment" + dots,
-                            "-_-",
-                            msg.nick + " pls",
-                            "i cry",
-                            "fml",
-                            "?",
-                            ".",
-                            "/join 0",
-                            "/part SCREW U GUYS IM LEAVING AND NEVER COMING "
-                                "BACK AGAIN!! IT'S ALL %s'S FAULT I FKN HATE "
-                                "YOU ALL \x02</3" % msg.nick.upper(),
-                            "stop highlighting me!",
-                            "\x02%s\x02 added to ignore list." % msg.nick,
-                            "!votekline " + msg.nick]
-            wowResponses2 = ["kicks ", "stabs ", "fites ", "bans "]
-            brenden = ["stfu blossom", "stfu brenda", "~~brenda blossom~~"]
-            n = random.randint(-5, 101)
-            #if n >= 96 and msg.nick.lower().startswith("brend"):
-            #    irc.queueMsg(ircmsgs.privmsg("BotServ", "say {} {}".format(msg.args[0],random.choice(brenden))))
-            if n >= 42:
-                irc.queueMsg(ircmsgs.privmsg("BotServ", "say {} {}".format(msg.args[0],random.choice(wowResponses1))))
-            elif n >= 21:
-                irc.queueMsg(ircmsgs.privmsg("BotServ", "act {} {}".format(msg.args[0],random.choice(wowResponses2)+msg.nick)))
+            volatile = ["kicks ", "stabs ", "fites ", "bans ", "ddas ", "packets ", "beats "]
+            exclaim = (("!" * random.randint(1,5)) + ("1" * random.randint(0,2))) * \
+                random.randint(1,2) + ("!" * random.randint(-1,5))
+            gemotes = ["xD", "=']", "\\o/", ":"+"3"*random.randint(1,4), "^_^"]
+            bemotes = ["-_-", ":|", ":\\"]
+            if irc.network.lower() == "overdrive-irc":
+                if "wow" in irc.state.channels[msg.args[0]].ops and \
+                    ircutils.stripFormatting(msg.args[1].lower()).startswith("wow"):
+                    wowResponses1 = ["what is it",
+                                    "hi %s%s" % (msg.nick, dots),
+                                    "o/",
+                                    "HI %s%s" % (msg.nick.upper(), dots),
+                                    "go away "+random.choice(bemotes),
+                                    "FFS"+random.choice(bemotes),
+                                    "ffs i'm trying to work",
+                                    "WHAT DO YOU WANT",
+                                    "leave me alone "+random.choice(bemotes),
+                                    "ugh",
+                                    "pls",
+                                    "stop" + dots,
+                                    "stop it!",
+                                    "reproted to fbi for harrassment" + dots,
+                                    "-_-",
+                                    msg.nick + " pls",
+                                    "i cry",
+                                    "fml",
+                                    "?",
+                                    ".",
+                                    "/join 0",
+                                    "/part SCREW U GUYS IM LEAVING AND NEVER COMING "
+                                        "BACK AGAIN!! IT'S ALL %s'S FAULT I FKN HATE "
+                                        "YOU ALL \x02</3" % msg.nick.upper(),
+                                    "stop highlighting me!",
+                                    "\x02%s\x02 added to ignore list." % msg.nick,
+                                    "!votekline " + msg.nick]
+                    # brenden = ["stfu blossom", "stfu brenda", "~~brenda blossom~~"]
+                    n = random.randint(-5, 101)
+                    #if n >= 96 and msg.nick.lower().startswith("brend"):
+                    #    irc.queueMsg(ircmsgs.privmsg("BotServ", "say {} {}".format(msg.args[0],random.choice(brenden))))
+                    if n >= 42:
+                        irc.queueMsg(ircmsgs.privmsg("BotServ", "say {} {}".format(msg.args[0],random.choice(wowResponses1))))
+                    elif n >= 21:
+                        irc.queueMsg(ircmsgs.privmsg("BotServ", "act {} {}".format(msg.args[0],random.choice(volatile)+msg.nick)))
+                elif msg.nick.lower().startswith("brend"):
+                    bad = ["chink", "dog fried", "nigr", "nigger", "chinq"]
+                    for w in bad:
+                        if w in ircutils.stripFormatting(msg.args[1].lower()):
+                            irc.queueMsg(ircmsgs.kick(msg.args[0], msg.nick, "RACIST"))
+                        return
+                    alsobad = ["veggie tales", 'whore', 'wh0re']
+                    for w in alsobad:
+                        if w in ircutils.stripFormatting(msg.args[1].lower()):
+                            irc.queueMsg(ircmsgs.kick(msg.args[0], msg.nick, "nothx"))
+                elif ircutils.stripFormatting(msg.args[1]) == ".":
+                    dotresponses = ["r u mad?", "lol r u mad", "mmm dots", ",", "no spam pls" + dots, ":D", "ok"]
+                    if len(self.dotCounter) >= 4:
+                        r = random.random()
+                        if r >= 0.5:
+                            irc.queueMsg(ircmsgs.privmsg(msg.args[0], random.choice(dotresponses)))
+                    else: self.dotCounter.enqueue([0])
+                elif ircutils.stripFormatting(msg.args[1]) == "ok":
+                    okresponses = ["not ok", "ok", "ko", "not ok"+unichr(2122), "no spam", "^why does everyone say that ._."]
+                    r = random.randint(1, 23)
+                    if r >= 17:
+                        irc.queueMsg(ircmsgs.action(msg.args[0], random.choice(volatile)+msg.nick))
+                    elif r >= 6:
+                        irc.queueMsg(ircmsgs.privmsg(msg.args[0], random.choice(okresponses)))
+            if irc.network.lower() in ["overdrive-irc", "stripechat"] and \
+                "brendi hitler blossom" in ircutils.stripFormatting(msg.args[1].lower()):
+                irc.queueMsg(ircmsgs.privmsg(msg.args[0], msg.nick + ": the entire topic changes" + exclaim))
+            if irc.network.lower() == "stripechat":
+                r = random.random()
+                if msg.args[1].lower().startswith("&topic") and "hackinbot" in msg.args[1].lower() \
+                    and r >= 0.3:
+                    irc.queueMsg(ircmsgs.privmsg(msg.args[0], "OH, hackinbot! " + random.choice(gemotes)))
+                    
+                    
 
 
 Class = Randomness
