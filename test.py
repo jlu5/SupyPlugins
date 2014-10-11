42058a81ce7ec73d0a48e037e16fd5a316ab5b17
spline <reticulatingspline@github.com>
2014-10-11 11:11:21 -0400

    Add in more tests.

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ from supybot.test import *
 
 class WeatherTestCase(PluginTestCase):
     plugins = ('Weather',)
-    #config = {'supybot.plugins.Weather.apiKey':'fc7cb609a45365fa'}
 
     def setUp(self):
         PluginTestCase.setUp(self)
@@ -18,6 +17,8 @@ class WeatherTestCase(PluginTestCase):
     def testWeather(self):
         self.assertSnarfResponse('reload Weather', 'The operation succeeded.')
         self.assertRegexp('wunderground 10002', 'New York, NY')
-
+        self.assertSnarfResponse('setweather 10002', "I have changed test's weather ID to 10002")
+        self.assertSnarfResponse('setuser metric True', "I have changed test's metric setting to 1")
+        self.assertRegexp('wunderground', 'New York, NY')
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
