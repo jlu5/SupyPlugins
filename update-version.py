d940878ae82f5ab187748b3d50a3cb8ed8fb9b73
James Lu <GLolol1@hotmail.com>
2015-10-02 22:41:34 -0700

    Overhaul update-version.py using argparse
    Setting specific versions, choosing which plugin folders to update, and disabling auto-commit are now command line options.

--- update-version.py
+++ update-version.py
@@ -33,32 +33,58 @@ import os
 import sys
 import re
 import time
+import argparse
 
 subtrees = []
 gitlog = subprocess.check_output(("git", "log"))
 
-try:
-    version = sys.argv[1].lower()
-except IndexError:
-    version = time.strftime("%Y.%m.%d")
+default_version = time.strftime("%Y.%m.%d")
+
+parser = argparse.ArgumentParser(description="Script to batch update plugins' versions in their __init__.py's")
+parser.add_argument('-v', "--version", help='the version to upgrade to - if not defined, defaults to the current date (YYYY.MM.DD)"', default=default_version, nargs="?")
+parser.add_argument("-n", "--no-commit", help="Don't commit and tag after incrementing version", action='store_true')
+parser.add_argument("-f", "--folders", help="Sets the plugin folders to look through", nargs='*', default=os.listdir())
+args = parser.parse_args()
+
+version = args.version
 
 for line in gitlog.decode("utf-8").split('\n'):
     splitline = line.split("git-subtree-dir: ", 1)
     if len(splitline) == 2:
         subtrees.append(splitline[1])
 
-for dir in filter(os.path.isdir, os.listdir()):
+to_commit = []
+
+version_re = re.compile(r'__version__ = "(.*?)"')
+for dir in filter(os.path.isdir, args.folders):
     name = os.path.join(dir, "__init__.py")
     # Don't overwrite subtrees! Otherwise, we'll get sync problems really easily...
     if dir in subtrees or not os.path.exists(name):
         continue
-    print('Rewriting %s __version__: %s' % (name, version))
-    # Read, replace, go back to the file's beginning, and write out the updated contents.
-    with open(name, 'r+') as f:
-        contents = f.read()
-        contents = re.sub(r'__version__ = ".*?"', '__version__ = "%s"' % version, contents)
-        f.seek(0)
-        f.write(contents)
+    try:
+        # Read, replace, go back to the file's beginning, and write out the updated contents.
+        with open(name, 'r+') as f:
+            contents = f.read()
+            old_version = version_re.search(contents).group(1)
+            new_version = version
+            # If the existing version doesn't follow our format (YYYY.mm.dd), append it with a +.
+            # But only do this once...
+            if old_version.count('.') != 2 and version == default_version:
+                old_version = old_version.split('+', 1)[0]
+                new_version = "%s+%s" % (old_version, version)
+            print('Rewriting %s __version__: %s' % (name, new_version))
+            contents = version_re.sub('__version__ = "%s"' % new_version, contents)
+            f.seek(0)
+            f.truncate()
+            f.write(contents)
 
-subprocess.call(('git', 'commit', '-am', 'Bump version to ' + version))
-subprocess.call(('git', 'tag', version))
+            f.close()
+    except:
+        pass
+    else:
+        to_commit.append(name)
+
+if not args.no_commit:
+    print("Automatically committing and tagging as %s." % version)
+    subprocess.call(['git', 'commit', '-m', 'Bump version to %s' % version] + to_commit)
+    subprocess.call(('git', 'tag', version))
