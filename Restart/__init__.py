11a03ad9a0fef84cb3c18aa78d708921ce9a4c90
James Lu <james@overdrivenetworks.com>
2017-05-31 13:10:47 -0700

    Bump version to 2017.05.31

--- Restart/__init__.py
+++ Restart/__init__.py
@@ -35,7 +35,7 @@ Restart plugin: provides a command to restart Limnoria from IRC.
 import supybot
 import supybot.world as world
 
-__version__ = ""
+__version__ = "2017.05.31"
 __author__ = supybot.Author('James Lu', 'GLolol', 'james@overdrivenetworks.com')
 __contributors__ = {}
 __url__ = 'https://github.com/GLolol/SupyPlugins/'
