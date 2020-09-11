f2d68dcceb162d8449496a8c4cffc9a61fdcb66d
mogad0n <melodon@tuta.io>
2020-09-11 07:19:54 +0530

    Making the output for nowplaying immersive
    Added a simple conditional to make the output of now playing a little more aesthetically pleasing.
    mogad0n is listening to Roller Mobster by Carpenter Brut [Trilogy]
    vs
    mogad0n listened to Roller Mobster by Carpenter Brut [Trilogy] just now.
    Close #92

--- LastFM/plugin.py
+++ LastFM/plugin.py
@@ -148,8 +148,12 @@ class LastFM(callbacks.Plugin):
             except KeyError:
                 pass
 
-        s = '%s listened to %s by %s %s %s%s. %s' % (ircutils.bold(user), ircutils.bold(track),
-            ircutils.bold(artist), album, time, ext_info, public_url)
+        if time == 'just now':
+            s = '%s is listening to %s by %s %s %s. %s' % (ircutils.bold(user), ircutils.bold(track),
+                ircutils.bold(artist), album, ext_info, public_url)
+        else:
+            s = '%s listened to %s by %s %s %s%s. %s' % (ircutils.bold(user), ircutils.bold(track),
+                ircutils.bold(artist), album, time, ext_info, public_url)
         irc.reply(utils.str.normalizeWhitespace(s))
 
     @wrap(["something"])
